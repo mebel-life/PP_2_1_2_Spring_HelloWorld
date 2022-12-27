2a37797f77420bcc96e54b48814923d866064001
Mihail <quickautotrans@gmail.com>
2022-12-27 19:58:50 +0300

    done

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,14 +1,26 @@
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import sun.reflect.generics.tree.DoubleSignature;
 
 @Configuration
 public class AppConfig {
  
     @Bean(name="helloworld")
+    @Scope ("singleton")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Bean(name="cat")
+    @Scope ("prototype")
+    public Cat getCat() {
+        Cat cat= new Cat();
+        cat.setMessage("мяу-мяу!");
+        return cat;
+    }
+
+
 }
